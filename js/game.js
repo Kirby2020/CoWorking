57a645b57de8933505cc549d0750cc7cb2c4bf70
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2021-04-17 16:52:06 +0200

    cleanup
    Added assets
    Added dependancies: socket.io,  express
    Background loaded

--- js/game.js
+++ js/game.js
@@ -1,4 +1,21 @@
 console.log("Hello World");
+function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
 
-let gameWindow = document.getElementById("gameWindow");
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    })
+}
 
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+
+loadImage('./assets/images/backgrounds/background1.jpg')
+.then(image => {
+    context.drawImage(image, 0, 0);
+
+})
