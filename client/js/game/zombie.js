2de336f41521a9bf3c3ddc53e5d39095bc832b37
Marjolein <vuurtjevlam@gmail.com>
2021-04-28 15:59:28 +0200

    plant/zombie klasse ruw

---
+++ client/js/game/zombie.js
@@ -0,0 +1,43 @@
+class zombie {
+
+
+    icon() {
+        this.icon=icon;
+    }
+
+    sprite() {
+        this.sprite=sprite;
+    }
+
+    animation(stand, walk, run, die) {
+        this.animation();
+    }
+
+    health() {
+        this.health=health;
+    }
+
+    attack_damage() {
+        this.attack_damage=attack_damage;
+    }
+
+    attack_speed() {
+        this.attack_speed=attack_speed;
+    }
+
+    walk_speed() {
+        this.walk_speed=walk_speed;
+    }
+
+    cost() {
+        this.cost=cost;
+    }
+
+    cooldown() {
+        this.cooldown=cooldown;
+    }
+
+    special(canJump, instaKill, impThrow, hasDoor, hasNewspaper) {
+        this.special();
+    }
+}
