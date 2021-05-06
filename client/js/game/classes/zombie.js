6fb5ca4e32d9d140859a7a626dede0601f2bf67b
Marjolein <vuurtjevlam@gmail.com>
2021-05-06 16:22:50 +0200

    zombie klasse

--- client/js/game/classes/zombie.js
+++ client/js/game/classes/zombie.js
@@ -1,4 +1,5 @@
 import { canvas, context, CELL_SIZE, gameGrid, seedBankGridPlants, seedBankGridZombies, CELL_GAP } from './constants.js';
+import {SEEDSLOT_SIZE} from "../constants";
 
 class zombie {
     constructor(x, y) {
@@ -6,57 +7,85 @@ class zombie {
         this.y = y;
         this.width = x * CELL_SIZE.width;
         this.height = y * CELL_SIZE.height;
+        this.seedslotWidth = x * SEEDSLOT_SIZE.width;
+        this.seedslotHeight = y * SEEDSLOT_SIZE.height;
         this.isShooting = false;
         this.projectiles = [];
         this.timer = 0;
     }
-}
-class zombieNormaal extends zombie {
-    constructor(x, y, attackDamage, attackSpeed, walkSpeed, cooldown, sprite,) {
-        super(x, y);
-        this.health = 270;
-        this.attackDamage = attackDamage;
-        this.attackSpeed = attackSpeed;
-        this.walkSpeed = walkSpeed;
-        this.cooldown = cooldown;
-        this.sprite = sprite;
+    draw(){
+
+    }
+    update(){
+
     }
 }
-class coneHead extends zombie{
-    constructor(x,y, attackDamage, attackSpeed, walkSpeed, cooldown, sprite) {
-        super(x,y);
+//cost in brain
 
-        this.health = 640;
-        this.attackDamage = attackDamage;
+class zombieNormaal extends zombie {
+    constructor(x, y) {
+        super(x, y);
+        this.health = 100;
+        this.attackDamage = attackDamage; //Geen idee wat dit is
         this.attackSpeed = attackSpeed;
-        this.walkSpeed = walkSpeed;
-        this.cooldown = cooldown;
+        this.walkSpeed = 2; //Ik vond online dat die er 4 stappen overdeed om 1 vakje te verlopen. Ik weet niet hoe snel dit juist is dus gok ik dit.
+        this.cooldown = 5;
+        this.cost = 50;
         this.sprite = sprite;
     }
 }
 class newsPaper extends zombie{
-    constructor(x,y, attackDamage, attackSpeed, walkSpeed, cooldown, sprite, hasNewspaper) {
+    constructor(x,y) {
         super(x, y);
 
         this.health = 420;
         this.attackDamage = attackDamage;
         this.attackSpeed = attackSpeed;
-        this.walkSpeed = walkSpeed;
+        this.walkSpeed = 2;
         this.cooldown = cooldown;
+        this.cost = 50;
         this.sprite = sprite;
-        this.hasNewspaper= hasNewspaper;
+        this.shield= 100;
     }
 }
-class screenDoor extends zombie{
-    constructor(x,y, attackDamage, attackSpeed, walkSpeed, cooldown, sprite, hasDoor) {
+class coneHead extends zombie{
+    constructor(x,y) {
         super(x,y);
 
-        this.health = 1000;
+        this.health = 200;
         this.attackDamage = attackDamage;
         this.attackSpeed = attackSpeed;
-        this.walkSpeed = walkSpeed;
+        this.walkSpeed = 2;
         this.cooldown = cooldown;
+        this.cost = 75;
         this.sprite = sprite;
-        this.hasDoor= hasDoor;
+    }
+}
+class bucketHead extends zombie{
+    constructor(x,y) {
+        super(x,y);
+
+        this.health = 400;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.walkSpeed = 2;
+        this.cooldown = cooldown;
+        this.cost = 100;
+        this.sprite = sprite;
+    }
+}
+
+class poleVaulting extends zombie{
+    constructor(x,y) {
+        super(x, y);
+
+        this.health = 420;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.walkSpeed = 1;
+        this.cooldown = cooldown;
+        this.cost = 100;
+        this.sprite = sprite;
+        this.jump= jump;
     }
 }
